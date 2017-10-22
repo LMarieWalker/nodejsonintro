2dae744f967a731a35fe4082f4dd079313e1b42b
Rod <LourdesMarieWalker@gmail.com>
2017-10-21 21:53:59 -0700

    changes

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 8000;
-app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.json({ extended: true })); // you can accept json - parses to all types of requests
 
+// POST 'NAME', 'LAST', 'AGE'
 app.post('/create', function(req,res){
-  console.log(req.body);
+  console.log(req.body); //
   res.sendStatus(200)
 });
 
+// TAKE THE AGE FROM ABOVE AND DOUBLE IT
+app.post('/addage', function(req, res){
+  let doubledAge = req.body.age * 2;
+  res.json(doubledAge);
+  res.sendStatus(200);
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
